700a8f0c59f9e17669dd18e6650269c334b47e3f
jmesyan <jieqiang1017@qq.com>
2017-06-14 06:01:31 +0800

    no message

--- app/operates/MsgTip.go
+++ app/operates/MsgTip.go
@@ -2,45 +2,15 @@ package operates
 
 import (
 	"fmt"
-	// "github.com/andlabs/ui"
-	"log"
-
-	"github.com/sciter-sdk/go-sciter"
-	"github.com/sciter-sdk/go-sciter/window"
 )
 
 type MsgTip struct {
 }
 
 func (op *MsgTip) Execute(params map[string]string, lastRes interface{}) interface{} {
-
 	if msg, ok := params["msg"]; ok {
 		fmt.Println(msg)
-		go func() {
-			w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG|sciter.SW_POPUP, nil)
-			if err != nil {
-				log.Fatal("Create Window Error: ", err)
-			}
-			w.LoadFile("index.html")
-			// setEventHandler(w)
-			w.Show()
-			w.Run()
-		}()
-
-		// js.Global.Call("alert", msg)
-		// go func() {
-		// 	err := ui.Main(func() {
-		// 		w := ui.NewWindow("消息提醒", 400, 300, false)
-		// 		w.OnClosing(func(*ui.Window) bool {
-		// 			ui.Quit()
-		// 			return true
-		// 		})
-		// 		ui.MsgBox(w, "消息提示", msg)
-		// 	})
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-		// }()
+		WinMsg <- []byte(msg)
 
 		return true
 	} else {
