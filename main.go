f58244945c969e891230d3c812599be126c4556c
yanjq <yanjq@realbull.net>
2017-06-21 12:24:20 +0800

    no message

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import (
 	"flashCoder/app/controllers"
 	"flashCoder/app/kernel/crontab"
 	"flashCoder/utils"
-	// "log"
-	// "fmt"
+	"github.com/go-ini/ini"
 	"net/http"
 	"time"
 )
@@ -13,16 +12,25 @@ import (
 func main() {
 	//启动事件监听
 	crontab.Watching()
+	//获取server配置
+	config, err := ini.Load(utils.GetRootDirectory() + "/.env")
+	utils.CheckError(err)
+	servercf, err := config.GetSection("server")
+	utils.CheckError(err)
+	serverPort := "6339"
+	if servercf.HasKey("port") {
+		serverPort = servercf.Key("port").String()
+	}
 	//启动服务器
 	var Handler http.Handler
 	Handler = new(controllers.Controller)
 	s := &http.Server{
-		Addr:           ":6339",
+		Addr:           ":" + serverPort,
 		Handler:        Handler,
 		ReadTimeout:    100 * time.Second,
 		WriteTimeout:   100 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	utils.CheckError(err)
 }
